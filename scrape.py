df790bec7b4c6cccba4e084be184d00fe07359be
himal <himal9626@gmail.com>
2025-10-03 12:49:44 +0530

    Remove reload option from uvicorn run command in main execution block

--- scrape.py
+++ scrape.py
@@ -361,4 +361,4 @@ if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "cli":
         main()
     else:
-        uvicorn.run("scrape:app", host="0.0.0.0", port=8000, reload=True)
+        uvicorn.run("scrape:app", host="0.0.0.0", port=8000)
